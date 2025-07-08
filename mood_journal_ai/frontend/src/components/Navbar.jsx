6689e6d9df2f9474ab60f2ff94ff3e18fc836318
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-07-08 11:00:52 +1000

    navbar changes

--- mood_journal_ai/frontend/src/components/Navbar.jsx
+++ mood_journal_ai/frontend/src/components/Navbar.jsx
@@ -1,44 +1,18 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // optional for styling
+import "./Navbar.css"; 
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/journal">Journal</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/tips">Tips</Link></li>
-      </ul>
+        <div className="navbar-left">
+            <h1>Mood Journal AI</h1>
+        </div>
+        <div className="navbar-right">
+            <Link to="/">Home</Link>
+            <Link to="/journal">Journal</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/tips">Tips</Link>
+        </div>
     </nav>
   );
 }
-
-
-/*import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContextInstance";
-
-export default function Navbar() {
-    const { token, logout } = useContext(AuthContext);
-    return (
-    <nav>
-        <Link to ="/">Home</Link>  |
-        {token ? (
-            <>
-                <Link to="/journal">Journal</Link>  |
-                <Link to="/dashboard">Dashboard</Link>  |
-                <Link to="/tips">Tips</Link>  |
-                <button onCLick={logout}>Logout</button>
-            </>
-        ) : (
-            <>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-            </>
-        )}
-    </nav>
-    );
-}*/
