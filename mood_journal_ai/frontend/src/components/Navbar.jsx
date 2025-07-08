4e244e97359b4d1b770a1e1eb3f491fa15f04cfd
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-07-08 12:18:40 +1000

    navbar done

--- mood_journal_ai/frontend/src/components/Navbar.jsx
+++ mood_journal_ai/frontend/src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css"; 
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-        <div className="navbar-left">
-            <h1>Mood Journal AI</h1>
-        </div>
-        <div className="navbar-right">
-            <Link to="/">Home</Link>
-            <Link to="/journal">Journal</Link>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/tips">Tips</Link>
-        </div>
+      <div className="navbar-left">
+        <h1>Mood Journal AI</h1>
+      </div>
+      <div className="navbar-right">
+        <Link to="/">Home</Link>
+        <Link to="/journal">Journal</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/tips">Tips</Link>
+      </div>
     </nav>
   );
 }
