42feb8e6f40e744deb53a8c9180c96213e543c41
Unnati Gadhiya <unnatigadhiya0206@gmail.com>
2025-07-09 11:07:14 +1000

    home page done

--- mood_journal_ai/frontend/src/pages/Home.jsx
+++ mood_journal_ai/frontend/src/pages/Home.jsx
@@ -1,15 +1,13 @@
-//import { useState } from "react";
 import Login from "./Login.jsx";
 import Register from "./Register.jsx";
 import "./Home.css";
 
 export default function Home() {
-  //const [activeForm, setActiveForm] = useState(null);
 
   return (
     <div className="home-page">
       <div className="form-wrapper">
-        
+
         <div className="login-box">
             <h2>Login</h2>
             <Login />
@@ -21,18 +19,5 @@ export default function Home() {
           </div>
         </div>
     </div>
-
-    /*<div className="auth-section">
-        <div className="auth-toggle-buttons">
-          <button onClick={() => setActiveForm("login")}>Login</button>
-          <button onClick={() => setActiveForm("register")}>Register</button>
-        </div>
-
-        <div className="form-box">
-          {activeForm === "login" && <Login />}
-          {activeForm === "register" && <Register />}
-        </div>
-      </div>
-    </div> */
   );
 }
