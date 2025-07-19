e7fc0c5d67804542621a92e0ee57cd2aba8e840b
Unnati <unnatigadhiya0206@gmail.com>
2025-07-19 12:30:00 +1000

    Update main.jsx

--- mood_journal_ai/frontend/src/main.jsx
+++ mood_journal_ai/frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-//import { AuthProvider } from './context/AuthContext.jsx';
 import AuthProvider from './context/AuthContext.jsx';
 
 // This is the entry point for the React application.
